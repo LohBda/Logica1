58b8a045eb25c22bd73cb26c2635748023d5e8bc
Loren Daniela Hueso Barrera <danielahueso@gmail.com>
2023-03-21 23:39:41 -0500

    Gracias a todos los que hicieron esto posible

---
+++ Quinto.py
@@ -0,0 +1,13 @@
+print("Buenos días ¿Quieres probar la nueva forma innovadora de dar la oprtunida de sumar?")
+primerapalabra = "uy"
+segundapalara = "sikas"
+print(primerapalabra+segundapalara)
+print("ya no más:")
+unnumero = 5
+otronumero = 6
+print(unnumero+otronumero)
+print("ahora llega... ")
+unnumero = int(input())
+otronumero = int(input())
+print(unnumero+otronumero)
+print("espero les guste amigos")
